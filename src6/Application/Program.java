cbd27bff4399697392e9bf5cf772dda10e265585
FabricioANS <fabricio.sousa.20@hotmail.com>
2025-08-18 22:11:44 -0300

    Ajustes

--- src6/Application/Program.java
+++ src6/Application/Program.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 public class Program {
 	
 	public static void main (String[] args) {
-		
 		// Define o idioma padrão para a localização, garantindo que números de ponto flutuante sejam lidos com ponto (.)
 		Locale.setDefault(Locale.US); 
 		// Cria um objeto Scanner para receber dados do teclado
